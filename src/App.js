e19af77257614df1911d4b68ec2adc71eb2306d2
Fzrabbi <31000643+Fzrabbi@users.noreply.github.com>
2020-08-31 23:23:31 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class='hero'>
+        <div class='wall'></div>
+        <div class='stars'></div>
+        <div class='robo'>
+          <img src={require('../src/robo.png')} />
+        </div>
+        <div class='weels'>
+          <img src={require('../src/weels.png')} className='frontweel'/>
+          <img src={require('../src/weels.png')} className='backweel'/>
+        </div>
+      </div>
+
     </div>
   );
 }
