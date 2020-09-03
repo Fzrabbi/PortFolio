c531a04ff9b39e9f58d87a45c5c49322dffdd1f8
Fzrabbi <31000643+Fzrabbi@users.noreply.github.com>
2020-09-03 12:51:07 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,52 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
+  document.body.onkeydown = function(e) {
+    if( e.keyCode == '39' ) {
+      console.log("space");
+      var all = document.getElementsByClassName('stars');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="running";
+      }
+      var all = document.getElementsByClassName('backweel');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="running";
+      }
+      var all = document.getElementsByClassName('frontweel');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="running";
+      }
+      var all = document.getElementsByClassName('wall');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="running";
+      }
+    }
+    else if( e.keyCode == '37' ) {
+      console.log("a");
+      var all = document.getElementsByClassName('stars');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="paused";
+      }
+      var all = document.getElementsByClassName('backweel');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="paused";
+      }
+      var all = document.getElementsByClassName('frontweel');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="paused";
+      }
+      var all = document.getElementsByClassName('wall');
+      for (var i = 0; i < all.length; i++) {
+        all[i].style.animationPlayState="paused";
+      }
+    }
+  }
+ 
+    
+  
+  
   return (
+    
     <div>
       <div class='hero'>
         <div class='wall'></div>
@@ -17,8 +62,8 @@ function App() {
           <img src={require('../src/weels.png')} className='backweel'/>
         </div>
       </div>
-
     </div>
+    
   );
 }
 
